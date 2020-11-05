aaafa891dd501d559383c1138d3d487037a9e5fe
Matt Maslen <mattmaslen123@gmail.com>
2020-11-05 12:20:35 +0000

    q1 working version

---
+++ app/Person.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+
+class Person 
+{
+    private $firstName;
+    private $lastName;
+
+    public function __construct($firstName, $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+   
+    public function getFullName()
+    {
+        return "{$this->firstName} {$this->lastName}"; 
+    }
+
+    public function sayHelloTo($a)
+    {
+        return "Hello " . $a->getFullName();
+    }
+};
